8659524387badf04142359583418ff1f110636d9
yoelngl <yoelmarket123@gmail.com>
2022-05-18 21:48:45 +0700

    Change Backend

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,8 +18,9 @@ class UserController extends Controller
     }
 
     public function delete($id){
+        $user = User::where('id',$id)->first();
         $data = User::whereId($id)->delete();
-        return response()->json(['data' => $data,'message' => 'Data berhasil dihapus!']);
+        return response()->json(['data' => $user,'message' => 'Data berhasil dihapus!']);
     }
 
     public function store(){
@@ -42,13 +43,15 @@ class UserController extends Controller
             'address' => request()->address,
             'major' => request()->major,
         ];
+
         try{
             if(request()->id){
                 User::whereId(request()->id)->update($data);
-                return response()->json(['status' => true, 'message' => 'Data Mahasiswa berhasil diubah!']);
+                $user = User::where('id', request()->id)->first();
+                return response()->json(['status' => true, 'data' =>  $user, 'message' => 'Data Mahasiswa berhasil diubah!']);
             }
-            User::create($data);
-            return response()->json(['status' => true, 'message' => 'Data Mahasiswa berhasil ditambahkan']);
+            $data = User::create($data);
+            return response()->json(['status' => true,'data' =>  $data, 'message' => 'Data Mahasiswa berhasil ditambahkan']);
         }catch(\Throwable $e){
             return response()->json(['status' => false, 'message' => $e->getMessage()]);
         }
